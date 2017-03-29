d8e31254e56194cb031ea3c4805940e1b4a155ba
jinsuk3377 <jinsunk3377@naver.com>
2017-03-30 01:36:04 +0900

    1.0.2

--- EEBO/Ui_Main.py
+++ EEBO/Ui_Main.py
@@ -15,6 +15,92 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+class MyMplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+
+    def __init__(self, data, column_name, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+
+        self.data = data
+        self.column_name = column_name
+        self.compute_initial_figure()
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+class HeatMapMplCanvas (MyMplCanvas):
+    
+    def compute_initial_figure(self):
+        dt = self.data
+        column_name = self.column_name
+            
+        df_in = dt[column_name].reset_index()
+        z_day = dt[column_name].reset_index(drop=True)
+        
+        the_months, the_days, Z_dayXmonth = self.dates_to_dayXmonth(df_in, z_day)
+        # the_months = x-axis, the_days = y-axis, Z_dayXmonth = data
+        
+        # XAxistitle='Months in sample'
+        # YAxistitle='Days of the month', # y-axis title
+        
+        extent = [the_months[0], the_months[-1], the_days[0], the_days[-1]]
+        
+        self.axes.set_title(column_name)
+        self.axes.imshow(Z_dayXmonth, interpolation='none', extent=extent, origin='low', aspect=0.25)
+        #self.axes.colorbar()
+        
+    def dates_to_dayXmonth(self, df_in, z_in):
+        
+        # http://stackoverflow.com/questions/31613018/datetime64ns-to-timestamp-string-in-pandas
+        dates = pd.DatetimeIndex(df_in['Date']).to_native_types()
+        
+        # (1.1) List of months for all item in dates 
+        months = np.array([datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%m')
+                           for date in dates])
+        
+        # (1.2) Find indices of first day of the months
+        _, ind_tmp = np.unique(months, return_index=True)  # -> array([90,212,31,0, ...])
+        the_months_ind = np.sort(ind_tmp).tolist()  # -> array([0,31,59,90, ...])
+    
+        # (1*) Use these indices to make list months' name
+        the_months = months[the_months_ind].astype(np.int)
+        N_the_months = len(the_months)  # 8 months, in our case, from Jan to Aug
+        
+        # (2*) Make list of days of the month 
+        N_the_days = 31
+        the_days = np.arange(1, N_the_days + 1)  # [1, ..., 31]
+    
+        # (3.1) Make tmp array filled with NaNs
+        Z_tmp = np.empty((N_the_days, N_the_months))
+        Z_tmp.fill(np.nan)
+    
+        # (3.2) Make list of indices to fill in Z_tmp month by month
+        fill_i = the_months_ind + [len(months)]
+    
+        # (3.3) Loop through months
+        for i in range(N_the_months):
+            i0 = fill_i[i]  # get start
+            i1 = fill_i[i + 1]  #  and end index
+            delta_i = i1 - i0  # compute their difference (either 30,31 or 28)
+            Z_tmp[0:delta_i, i] = z_in[i0:i1]  # fill in rows of 
+    
+        # (3*) Copy tmp array to output variable
+        Z = Z_tmp
+    
+        return (the_months, the_days, Z)  # output coordinates for our plot
+
 
 class Ui_Main (QtWidgets.QMainWindow):
     
@@ -79,7 +165,7 @@ class Ui_Main (QtWidgets.QMainWindow):
         item = self.listWidget.item(2)
         item.setText(_translate("Form", "textlist3"))
         item = self.listWidget.item(3)
-        item.setText(_translate("Form", "textlist4"))
+        item.setText(_translate("Form", "textlist4")) 
         self.listWidget.setSortingEnabled(__sortingEnabled)
         self.radioButton.setText(_translate("Form", "Heatmap(default)"))
         self.radioButton_2.setText(_translate("Form", "Energy"))
@@ -109,52 +195,21 @@ class Ui_Main (QtWidgets.QMainWindow):
         return pd.concat(daily_df, axis=1)
 
     # https://plot.ly/python/heatmaps-contours-and-2dhistograms-tutorial/   
-    def dates_to_dayXmonth(self, df_in, z_in):
-        
-        # http://stackoverflow.com/questions/31613018/datetime64ns-to-timestamp-string-in-pandas
-        dates = pd.DatetimeIndex(df_in['Date']).to_native_types()
-        
-        # (1.1) List of months for all item in dates 
-        months = np.array([datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%m')
-                           for date in dates])
     
-        # (1.2) Find indices of first day of the months
-        _, ind_tmp = np.unique(months, return_index=True)  # -> array([90,212,31,0, ...])
-        the_months_ind = np.sort(ind_tmp).tolist()  # -> array([0,31,59,90, ...])
-    
-        # (1*) Use these indices to make list months' name
-        the_months = months[the_months_ind].astype(np.int)
-        N_the_months = len(the_months)  # 8 months, in our case, from Jan to Aug
-        
-        # (2*) Make list of days of the month 
-        N_the_days = 31
-        the_days = np.arange(1, N_the_days + 1)  # [1, ..., 31]
-    
-        # (3.1) Make tmp array filled with NaNs
-        Z_tmp = np.empty((N_the_days, N_the_months))
-        Z_tmp.fill(np.nan)
-    
-        # (3.2) Make list of indices to fill in Z_tmp month by month
-        fill_i = the_months_ind + [len(months)]
-    
-        # (3.3) Loop through months
-        for i in range(N_the_months):
-            i0 = fill_i[i]  # get start
-            i1 = fill_i[i + 1]  #  and end index
-            delta_i = i1 - i0  # compute their difference (either 30,31 or 28)
-            Z_tmp[0:delta_i, i] = z_in[i0:i1]  # fill in rows of 
-    
-        # (3*) Copy tmp array to output variable
-        Z = Z_tmp
-    
-        return (the_months, the_days, Z)  # output coordinates for our plot
- 
     def plot_Heatmap(self):
         width = 650  # plot width 
         height = 800  # plot height
         
         df_day = self.df_to_day()
+        
+        self.main_widget = QtWidgets.QWidget(self)
+
+        l = QtWidgets.QVBoxLayout(self.main_widget)
+        hc = HeatMapMplCanvas(df_day, df_day.columns[1])
+        l.addWidget(hc)
+        
         # Make heatmap object
+        """
         for i in range(len(df_day.columns)):
             column_name = df_day.columns[i]
             
@@ -176,6 +231,8 @@ class Ui_Main (QtWidgets.QMainWindow):
             
         plt.show()
         print("plot done")
+        """
+        
             
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
